9a211e8ee1bcc9c0ebfa34936dbd1229b74f8ddd
johnson <g00389668@atu.ie>
2022-11-24 13:58:24 +0000

    new methods added

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,7 +1,21 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
+        add();
+    }
 
+    private static void add() {
+        System.out.println("Please enter first number: ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNum = inputs.nextInt();
+
+        System.out.println("Please enter second number: ");
+        int secondNum = inputs.nextInt();
+
+        int total = firstNum + secondNum;
+        System.out.println("the total is " +total);
     }
 }
