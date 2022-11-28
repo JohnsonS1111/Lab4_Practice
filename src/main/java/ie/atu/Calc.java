585259519440084fa2c3bf976e0f69d4f68e05a1
johnson <G00389668@atu.ie>
2022-11-28 12:13:58 +0000

    new method divide(); added

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,7 +1,22 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
 
+        divide();
+    }
+
+    private static void divide() {
+        System.out.println("Enter first number");
+        Scanner inputs = new Scanner(System.in);
+        int firstNum = inputs.nextInt();
+
+        System.out.println("Please enter second number: ");
+        int secondNum = inputs.nextInt();
+
+        int total = firstNum/secondNum;
+        System.out.println("the total is " +total);
     }
 }
