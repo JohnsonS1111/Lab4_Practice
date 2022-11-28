48e8f9eaaf7a8e5cbaf8c8f78d120a23ba390d0c
johnson <G00389668@atu.ie>
2022-11-28 12:10:36 +0000

    new method delete(); added

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -3,5 +3,11 @@ package ie.atu;
 public class Calc {
     public static void main(String[] args) {
 
+        delete();
+    }
+
+    private static void delete() {
+        System.out.println("Enter first number");
+
     }
 }
